27add3fab17c8f6d0840d1c5bc9ef43035c79d71
Katur7 <grimurk@gmail.com>
2014-05-05 15:52:03 +0000

    first commit

---
+++ 3viknavinir/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_3viknavinir.Startup))]
+namespace _3viknavinir
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
