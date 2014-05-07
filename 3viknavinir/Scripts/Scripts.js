bd41525db2b34aee64f9c898b46d61332975a4bb
Katur7 <grimurk@gmail.com>
2014-05-07 18:29:26 +0000

    Accordion

--- 3viknavinir/Scripts/Scripts.js
+++ 3viknavinir/Scripts/Scripts.js
@@ -1,5 +1,5 @@
 ﻿$(document).ready(function () {
-    for (i = new Date().getFullYear() ; i > 1900; i--) {
-        $('#yearpicker').append($('<option />').val(i).html(i));
-    }
+    $(function () {
+        $("#accordion").accordion();
+    });
 });
