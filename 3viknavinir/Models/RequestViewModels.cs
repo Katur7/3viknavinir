bf0ac33c51e3484785ec78836bbede4a5f511316
Steinunn <stemfri@gmail.com>
2014-05-08 19:02:10 +0000

    Fixed new request

---
+++ 3viknavinir/Models/RequestViewModels.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace _3viknavinir.Models
+{
+    public class RequestViewModels
+    {
+        
+    }
+    public class NewRequestViewModel
+    {
+        [Required( ErrorMessage = "Slá verður inn nafn myndefnis" )]
+        [Display( Name = "Nafn Kvikmyndar" )]
+        public string movieName { get; set; }
+
+        [Required( ErrorMessage = "Slá verður inn ár sem  mynd var gefin út" )]
+        [Display( Name = "Ártal" )]
+        public uint yearOfRelease { get; set; }
+
+        [Display( Name = "IMDB ID" )]
+        public string imdbID { get; set; }
+    }
+}
