2605eeeac333b606fdd70987aa36f08298bccd79
erla1011 <erla1011@gmail.com>
2014-05-08 13:10:02 +0000

    Tab changes

---
+++ 3viknavinir/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _3viknavinir.Controllers
+{
+    public class CategoryController : Controller
+    {
+        //
+        // GET: /Category/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		public ActionResult Categories()
+		{
+			return View();
+		}
+	}
+}
