afa1ff5b26c1d3270904b686f7cfe6394874a95a
fanney22 <fanneyhrund@gmail.com>
2014-05-09 14:24:25 +0000

    I made very nice get all yes

--- 3viknavinir/Repo/UserRepo.cs
+++ 3viknavinir/Repo/UserRepo.cs
@@ -5,24 +5,33 @@ using System.Web;
 using _3viknavinir.Content;
 //Fanney
 namespace _3viknavinir.Repo
-{	public class UserRepo
-	{
-    private VERK014_H3Entities db = new VERK014_H3Entities();
-
-    public UserRepo GetUserByID(int id)
+{
+    public class UserRepo
     {
-        var user = ( from u in db.Users
-                        where u.id == id
+        private VERK014_H3Entities db = new VERK014_H3Entities();
+
+        public Users GetUserByID(int id)
+        {
+            var user = (from u in db.Users
+                        where u.Id == id
                         select u).FirstOrDefault();
-        return user;
-    }
-    public UserRepo GetUserByName(string name)
-    {
-        var user = ( from u in db.Users
-                    where u.name == name
-                    select u).FirstOrDefault();
-        return user;
-    }
+            return user;
+        }
+        public Users GetUserByName(string name)
+        {
+            var user = (from u in db.Users
+                        where u.name == name
+                        select u).FirstOrDefault();
+            return user;
+        }
 
-	}
+        public IEnumerable<Users> GetAllUsers()
+        {
+            IEnumerable<Users> allUsers = (from u in db.Users
+                                              select u);
+            return allUsers;
+                                               
+        }
+
+    }	
 }
