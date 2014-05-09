50e1acf1e654f54d10fdd746c8ede5a3438b294e
fanney22 <fanneyhrund@gmail.com>
2014-05-09 14:10:19 +0000

    user repo pretty get by actions

--- 3viknavinir/Repo/UserRepo.cs
+++ 3viknavinir/Repo/UserRepo.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using _3viknavinir.Content;
+//Fanney
 namespace _3viknavinir.Repo
-{
-	public class UserRepo
+{	public class UserRepo
 	{
+    private VERK014_H3Entities db = new VERK014_H3Entities();
+
+    public UserRepo GetUserByID(int id)
+    {
+        var user = ( from u in db.Users
+                        where u.id == id
+                        select u).FirstOrDefault();
+        return user;
+    }
+    public UserRepo GetUserByName(string name)
+    {
+        var user = ( from u in db.Users
+                    where u.name == name
+                    select u).FirstOrDefault();
+        return user;
+    }
+
 	}
 }
