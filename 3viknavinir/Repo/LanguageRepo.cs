2e0368f8a0155db511725a2b8df3733c93a2de27
Steinunn <stemfri@gmail.com>
2014-05-09 13:39:18 +0000

    Changed languagerepo

--- 3viknavinir/Repo/LanguageRepo.cs
+++ 3viknavinir/Repo/LanguageRepo.cs
@@ -1,11 +1,37 @@
-﻿using System;
+﻿using _3viknavinir.Content;
+using _3viknavinir.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace _3viknavinir.Repo
 {
-	public class LanguageRepo
-	{
-	}
+    public class LanguageRepo : VERK014_H3Entities
+    {
+        private VERK014_H3Entities db = new VERK014_H3Entities( );
+
+        public string GetLanguageByID(int id)
+        {
+            var language = (from l in db.Language
+                            where l.Id == id
+                            select l.name).FirstOrDefault();
+            return language;
+        }
+        public string GetLanguageByName( string name )
+        {
+            var language = ( from l in db.Language
+                             where l.name == name
+                             select l.name ).FirstOrDefault( );
+            return language;
+        }
+        public IEnumerable<Language> GetAllLanguages()
+        {
+            var all = from l in db.Language
+                      select l;
+                      return all;
+        }
+
+        
+    }
 }
