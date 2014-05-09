b7b6ad61d730a9ecae19ee21c6282a0fa20ac405
Katur7 <grimurk@gmail.com>
2014-05-09 11:45:46 +0000

    Comment works

--- 3viknavinir/Repo/CategoryRepo.cs
+++ 3viknavinir/Repo/CategoryRepo.cs
@@ -9,14 +9,18 @@ namespace _3viknavinir.Repo
 {
     public class CategoryRepo : VERK014_H3Entities
 	{
-		private ApplicationDbContext db = new ApplicationDbContext();
+		//public CategoryRepo()
+		//{
+		//	private ApplicationDbContext db = new ApplicationDbContext();
+		//}
+		
 
-		public IEnumerable<Category> GetAllCategories()
-		{
-			var all = from c in db.Categories
-					  select c;
-			return all;
-		}
+		//public IEnumerable<Category> GetAllCategories()
+		//{
+		//	var all = from c in db.Cate
+		//			  select c;
+		//	return all;
+		//}
         //public string GetCategoryByID( int id )
         //{
             
