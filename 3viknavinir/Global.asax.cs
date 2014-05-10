3144970d80aab60de1c17c06f7210ca8f3bc3ce0
Steinunn <stemfri@gmail.com>
2014-05-10 11:22:41 +0000

    Fixed login/register error in gloabal.asax

--- 3viknavinir/Global.asax.cs
+++ 3viknavinir/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using _3viknavinir.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +18,9 @@ namespace _3viknavinir
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Steinunn, Fixes error on login or register
+            Database.SetInitializer<_3viknaContext>(null);
         }
     }
 }
