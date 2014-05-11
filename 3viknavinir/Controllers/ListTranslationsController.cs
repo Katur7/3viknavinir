4a94d26611b811e6b2b3351a1e1e045b2dbec260
Katur7 <grimurk@gmail.com>
2014-05-11 17:31:35 +0000

    Fikt + skip on Textar A-Ö

--- 3viknavinir/Controllers/ListTranslationsController.cs
+++ 3viknavinir/Controllers/ListTranslationsController.cs
@@ -1,4 +1,5 @@
-﻿using _3viknavinir.Repo;
+﻿using _3viknavinir.Models;
+using _3viknavinir.Repo;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,22 +11,34 @@ namespace _3viknavinir.Controllers
 	[HandleError]
     public class ListTranslationsController : Controller
     {
+		const int ITEMSPERPAGE = 15;
+
         // GET: /ListTranslations/
         public ActionResult Index()
         {
             return View();
         }
 
-		public ActionResult AlphabetizedTexts()
+		public ActionResult AlphabetizedTexts(int? id)
 		{
 			using(MediaRepo mediarepo = new MediaRepo())
 			{
+				int realid = 0;
+				if(id.HasValue)
+				{
+					realid = id.Value;
+				}
+
 				var allmedia = (from m in mediarepo.GetAllMedia()
-                                orderby m.title ascending
-                                select m).ToList();
+								orderby m.title ascending
+								select m).ToList().Skip(realid * ITEMSPERPAGE).Take(ITEMSPERPAGE);
+
+				var viewModel = new AlphabetizedTextsViewmodel();
+				viewModel.allMedia = allmedia;
+
 				if(allmedia != null)
 				{
-					return View(allmedia);
+					return View(viewModel);
 				}
 			}
 			return View();
