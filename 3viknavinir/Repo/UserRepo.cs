a75063f630b6b04d5c3217411188e8682032b3b6
erla1011 <erla1011@gmail.com>
2014-05-12 22:03:08 +0000

    UserRepo Hauks

--- 3viknavinir/Repo/UserRepo.cs
+++ 3viknavinir/Repo/UserRepo.cs
@@ -1,4 +1,6 @@
 ﻿using _3viknavinir.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +10,16 @@ namespace _3viknavinir.Repo
 {
 	public class UserRepo : IDisposable
 	{
+		private _3viknaContext db = new _3viknaContext();
 
+		public IEnumerable<IdentityUser> GetAllUsers()
+		{
+			return from user in db.Users select user;
+		}
+
+		public void Dispose()
+		{
+			db.Dispose();
+		}
 	}
 }
