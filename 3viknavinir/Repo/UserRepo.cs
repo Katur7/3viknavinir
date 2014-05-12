714664b3dffd81ff956308d2a1849ea1764d91e3
erla1011 <erla1011@gmail.com>
2014-05-12 22:00:24 +0000

    UserRepo

---
+++ 3viknavinir/Repo/UserRepo.cs
@@ -0,0 +1,13 @@
+﻿using _3viknavinir.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _3viknavinir.Repo
+{
+	public class UserRepo : IDisposable
+	{
+
+	}
+}
