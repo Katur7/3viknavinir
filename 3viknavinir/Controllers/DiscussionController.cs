287107d5eb5219efbc963ab5b956b2ad487e0e1c
Katur7 <grimurk@gmail.com>
2014-05-13 23:05:02 +0000

    Discussion No Null, but code in html :(

--- 3viknavinir/Controllers/DiscussionController.cs
+++ 3viknavinir/Controllers/DiscussionController.cs
@@ -14,40 +14,40 @@ namespace _3viknavinir.Controllers
         // GET: /Discussion/
         public ActionResult Translation(int? id)
         {
-			using (DiscussionRepo discussionRepo = new DiscussionRepo())
+			if (id.HasValue)
 			{
-				if (id.HasValue)
-			    {
-                    DiscussionViewModel viewModel = new DiscussionViewModel();
-                    int realid = id.Value;
-                    if (discussionRepo.IsExistingID(realid))
-                    {
-
-
-						IEnumerable<Discussion> allDiscussions = (from d in discussionRepo.GetCommentByMediaID(realid)
-																  orderby d.dateAdded descending
-																  select d);
-						viewModel.discussions = allDiscussions;
-                        viewModel.isEmpty = false;
-                        if (allDiscussions != null)
-                        {
-                            return View(viewModel);
-                        }
-                        else
-                        {
-                            return View( );
-                        }
-                        //IEnumerable<Discussion> discussion = (from d in discussionRepo.GetCommentByMediaID(realid)
-                        //                                      select d).ToList();
-                    }
-					else
+				int realid = id.Value;
+				using (DiscussionRepo discussionRepo = new DiscussionRepo())
+				{
+					using( MediaRepo mediaRepo = new MediaRepo())
 					{
-                        viewModel.isEmpty = true;
-						return View(viewModel);
+						DiscussionViewModel viewModel = new DiscussionViewModel();
+
+						if (discussionRepo.IsExistingID(realid))
+						{
+							var allDiscussions = (from d in discussionRepo.GetCommentByMediaID(realid)
+												  orderby d.dateAdded descending
+												  select d);
+							viewModel.discussions = allDiscussions;
+							viewModel.media = mediaRepo.GetMediaByID(realid);
+							viewModel.isEmpty = false;
+
+							using( UserRepo userRepo = new UserRepo())
+							{
+								viewModel.userName = userRepo.GetUserNameByID()
+							}
+
+							return View(viewModel);
+						}
+						else
+						{
+							viewModel.isEmpty = true;
+							return View(viewModel);
+						}
 					}
                 }
-				return View();
             }
+			return HttpNotFound();
         }
 	}
 }
