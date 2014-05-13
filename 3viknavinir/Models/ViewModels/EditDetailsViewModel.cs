ccd45737610f8e84498e5082ecb615dfdc3b8273
Katur7 <grimurk@gmail.com>
2014-05-13 10:44:33 +0000

    Edit

---
+++ 3viknavinir/Models/ViewModels/EditDetailsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _3viknavinir.Models.ViewModels
+{
+	public class EditDetailsViewModel
+	{
+		public int ID { get; set; }
+		public string title { get; set; }
+
+		public int year { get; set; }
+
+		public string description { get; set; }
+
+		public IEnumerable<Category> categories { get; set; }
+
+		public string posterPath { get; set; }
+	}
+}
