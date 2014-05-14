263788e7b4c2cc65312f38f4a45ec1fcca1bf0f8
Katur7 <grimurk@gmail.com>
2014-05-14 23:08:27 +0000

    Fix

--- 3viknavinir/Scripts/Scripts.js
+++ 3viknavinir/Scripts/Scripts.js
@@ -9,23 +9,25 @@ $(document).ready(function () {
         newTranslationLineDiv.after().html(
             '<input data-val="true" data-val-number="The field ID must be a number." data-val-required="The ID field is required." id="item_ID" name="item.ID" type="hidden" value="1">' +  //Breyta í countID
             '<p class="chapterLabel"><label for="Kafli">Kafli</label></p>' +
-            '<input class="chapterTextBox valid" data-val="true" data-val-number="The field chapterNumber must be a number." data-val-required="The chapterNumber field is required." id="item_chapterNumber" name="item.chapterNumber" type="text" value="1">' +
+            '<input class="chapterTextBox valid" data-val="true" data-val-number="The field chapterNumber must be a number." data-val-required="The chapterNumber field is required." id="item_chapterNumber" name="item.chapterNumber" type="text" value="">' +
             '<span class="field-validation-valid" data-valmsg-for="item.chapterNumber" data-valmsg-replace="true"></span>' +
             '<p class="startTimeLabel"><label for="Byrjunart_mi">Byrjunartími</label></p>' +
-            '<input class="startTimeTextBox" id="item_startTime" name="item.startTime" type="text" value="01:02">' +
+            '<input class="startTimeTextBox" id="item_startTime" name="item.startTime" type="text" value="">' +
             '<span class="field-validation-valid" data-valmsg-for="item.startTime" data-valmsg-replace="true"></span>' +
             '<div>' +
                 '<p class="timeLabels">-&gt;</p>' +
             '</div>' +
             '<p class="endTimeLabel"><label for="Endat_mi">Endatími</label></p>' +
-            '<input class="endTimeTextBox" id="item_endTime" name="item.endTime" type="text" value="01:03">' +
+            '<input class="endTimeTextBox" id="item_endTime" name="item.endTime" type="text" value="">' +
             '<span class="field-validation-valid" data-valmsg-for="item.endTime" data-valmsg-replace="true"></span>' +
             '<p class="subtitleTextLabel"><label for="Texti">Texti</label></p>' +
-            '<input class="subtitleTextBox" id="item_subtitle" name="item.subtitle" type="text" value="Við erum bílar.">' +
+            '<input class="subtitleTextBox" id="item_subtitle" name="item.subtitle" type="text" value="">' +
             '<span class="field-validation-valid" data-valmsg-for="item.subtitle" data-valmsg-replace="true"></span>' +
             '</div>');
 
-        newTranslationLineDiv.insertAfter(this);
+        var editLinesDiv = $(".editLines")
+
+        newTranslationLineDiv.insertBefore(editLinesDiv);
     });
 
     $('.upvoteTranslation').click(function ( event ) {
