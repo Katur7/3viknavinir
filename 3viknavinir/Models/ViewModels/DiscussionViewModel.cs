26a951f463f387abf98691fd275492cbdc38de97
Katur7 <grimurk@gmail.com>
2014-05-14 11:48:13 +0000

    Discussion

---
+++ 3viknavinir/Models/ViewModels/DiscussionViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace _3viknavinir.Models
+{
+	using _3viknavinir.Models.ViewModels;
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Web;
+
+    public class DiscussionViewModel
+    {
+        public List<DiscussionUserViewModel> discussions { get; set; }
+		public Media media{ get; set; }
+
+		public bool isEmpty { get; set; }
+    }
+}
