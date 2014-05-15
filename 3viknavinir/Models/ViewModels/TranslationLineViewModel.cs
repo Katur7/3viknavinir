ee4e8f0666dd7b02a015b80b49d250d12072f695
Katur7 <grimurk@gmail.com>
2014-05-15 17:58:32 +0000

    ajax submit form and stuff

---
+++ 3viknavinir/Models/ViewModels/TranslationLineViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _3viknavinir.Models.ViewModels
+{
+	public class TranslationLineViewModel
+	{
+		public int chapterNumber;
+		public string startTime;
+		public string endTime;
+		public string subtitle;
+	}
+}
